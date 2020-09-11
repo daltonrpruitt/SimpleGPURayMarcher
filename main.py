Add box rotation; fix box normal retrieval
Also add in rotation/translation matrix utility functions.

main.py
@@ -40,12 +40,13 @@ class RayMarchingWindow(BasicWindow):
         self.prog['height'].value = self.wnd.height
         # self.prog['time'].value = 0
 
-        self.prog['sphere.center'].value = (0.0, 1.0, 8.0)
+        self.prog['sphere.center'].value = (2.0, 0.0, 8.0)
         self.prog['sphere.radius'].value = 1.0
         self.prog['sphere.color'].value = (1.0, 0.0, 0.0)
         self.prog['sphere.shininess'].value = 32.0
 
         self.prog['box_center'].value = (-1, 0, 8)
+        self.prog['box_rotation'].value = (0, 0, 0) # Degrees
 
         self.prog['back_color'].value = (0, 0.3, 0.9, 1.0)
 
@@ -55,7 +56,7 @@ class RayMarchingWindow(BasicWindow):
         self.prog['cam_pos'].value = (0, 2, -5)
 
         self.prog['using_point_light'] = True
-        self.prog['using_dir_light'].value = False
+        self.prog['using_dir_light'].value = True
 
         vertices = np.array([
             -1, -1,
@@ -79,7 +80,8 @@ class RayMarchingWindow(BasicWindow):
         self.vao.render()
         self.prog['sphere.center'].value = ( np.cos(time/2)*2, 0, 8.0 + np.sin(time/2) *2)  #np.cos(time), np.sin(time*1.5), 10.0 + np.sin(time/2) * 5, 0.5
         self.prog['box_center'].value = ( np.cos(time/2-np.pi)*2, 0, 8.0 + np.sin(time/2-np.pi ) * 2)  #np.cos(time), np.sin(time*1.5), 10.0 + np.sin(time/2) * 5, 0.5
-        #self.prog['cam_pos'].value = (0, 1+ np.sin(time/4)*0.5, -5)
+        self.prog['box_rotation'].value = (0, -time, 0)  #
+        #self.prog['cam_pos'].value = (0, 1+ np.sin(time)*0.7, -5)
 
 
 if __name__ == '__main__':